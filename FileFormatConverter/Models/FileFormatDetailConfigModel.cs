78ecd20744dad74d9f584286e64d6ad831ebb77d
yhsgw <yhsgw.n@gmail.com>
2014-04-20 14:31:21 +0900

    ファイルフォーマット詳細設定を追加

---
+++ FileFormatConverter/Models/FileFormatDetailConfigModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FileFromatConverter
+{
+	public class FileFormatDetailConfigModel
+	{
+		public FileFormatDetailConfigModel ()
+		{
+		}
+
+		public List<FileFormatDetailConfig> Find()
+		{
+			var result = new List<FileFormatDetailConfig> ();
+			result.Add(
+				new FileFormatDetailConfig {
+					FileId = @"00000001",
+					EscapeChar = @"\",
+					ItemCount = 5,
+					IdItemInfo = @"ID001",
+					IdItemColumn = 2,
+					FileName = @"splitFile1.log"
+				}
+			);
+			result.Add(
+				new FileFormatDetailConfig {
+					FileId = @"00000002",
+					EscapeChar = @"\",
+					ItemCount = 4,
+					IdItemInfo = @"ID002",
+					IdItemColumn = 2,
+					FileName = @"splitFile2.log"
+				}
+			);
+
+			return result;
+
+		}
+	}
+}
+
